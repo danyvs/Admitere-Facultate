526f32b45e3207ac336109f2cf89624d4bec757f
Daniel Sava <danyvs_99@yahoo.com>
2017-07-17 20:33:31 +0300

    Update Numere mari.cpp

--- Numere mari.cpp
+++ Numere mari.cpp
@@ -10,3 +10,32 @@ void atribValue(int vec[], int x) {
     }
 }
 
+// 2. Initializarea unui numar mare cu un alt numar mare
+void atribHuge(int dest[], int src[]) {
+    // dest este initializat cu src
+    for (int i = 0; i <= src[0]; ++ i)
+        dest[i] = src[i];
+}
+
+// 3. Compararea a 2 numere mari
+// - -1, daca vec1 < vec2
+// - 0, daca vec1 = vec2
+// = 1, daca vec1 > vec2
+int comp(int vec1[], int vec2[]) {
+    // se elimina zerourile nesemnificative
+    while (vec1[0] && !vec1[vec1[0]])
+        -- vec1[0];
+    while (vec2[0] && !vec2[vec2[0]])
+        -- vec2[0];
+    if (vec1[0] < vec2[0])
+        return -1;
+    if (vec1[0] > vec2[0])
+        return 1;
+    for (int i = vec1[0]; i; -- i) {
+        if (vec1[i] < vec2[i])
+            return -1;
+        if (vec1[i] > vec2[i])
+            return 1;
+    }
+    return 0;
+}
