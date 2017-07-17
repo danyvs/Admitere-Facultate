9f54c507ae5fa74dae3eeb859db65fe9f0f4bbd1
danyvs <danyvs_99@yahoo.com>
2017-07-17 09:26:37 +0300

    Update Cel mai mare divizor comun.cpp

--- Cel mai mare divizor comun.cpp
+++ Cel mai mare divizor comun.cpp
@@ -33,4 +33,6 @@ int gcd(int a, int b) {
 2. Se prefera varianta 3 de scriere a functiei datorita usurintei de scriere si a dimensiunii reduse.
 3. Varianta 3 se poate scrie si fara folosirea operatorului tertiar (?), prin inlocuirea acestuia cu un if.
 !!! 4. Cel mai mic multiplu comun a 2 numere se calculeaza dupa urmatoarea formula: cmmmc(a, b) = (a * b) / cmmdc(a, b).
+!!! 5. Pentru a verifica daca 2 numere sunt prime intre ele, se calculeaza valoarea cmmdc-ului: daca aceasta este 1, atunci numerele sunt
+prime intre ele; daca valoarea este diferita de 1, atunci numerele NU sunt prime intre ele.
 */
