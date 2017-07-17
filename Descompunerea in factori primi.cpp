72a02132da14547638d99972b8809bb94cb98972
Daniel Sava <danyvs_99@yahoo.com>
2017-07-17 09:52:20 +0300

    Create Descompunerea in factori primi.cpp

---
+++ Descompunerea in factori primi.cpp
@@ -0,0 +1,36 @@
+// Descompunerea unui numar in factori primi, in mod eficient: x = a1 ^ p1 + a2 ^ p2 + ... + ak ^ pk
+
+void primeFactors(int x) {
+    int d, cnt;
+    cnt = 0;
+    while (x % 2 == 0) {
+        ++ cnt;
+        x /= 2;
+    }
+    if (cnt) {
+        // prelucrare 2 la puterea cnt
+    } 
+    d = 3;
+    while (d * d <= x) {
+        cnt = 0;
+        while (x % d == 0) {
+            ++ cnt;
+            x /= d;
+        }
+        if (cnt) {
+            // prelucrare d la puterea cnt
+        } 
+        d += 2;
+    }
+    if (x != 1) {
+        // prelucrare x la puterea 1
+    }
+}
+
+/* Observatii:
+1. In algoritmul de mai sus, prelucrare poate insemna afisare, memorare etc.
+2. Pe baza descompunerii in factori primi se poate determina numarul divizorilor duma urmatoarea formula: numarul de divizori = 
+(p1 + 1) * (p2 + 1) * ... * (pk + 1)
+3. Pe baza descompunerii in factori primi se poate determina suma divizorilor dupa urmatoarea formula : suma divizorilor = 
+((a1 ^ (p1 + 1) - 1) / (a1 - 1)) * ((a2 ^ (p2 + 1) - 1) / (a2 - 1)) * ... * ((ak ^ (pk + 1) - 1) / ak - 1).
+*/
