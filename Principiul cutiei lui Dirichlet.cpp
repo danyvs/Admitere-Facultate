3dafaad541f053e047aaaca4d6770c7454fa7a20
Daniel Sava <danyvs_99@yahoo.com>
2017-07-17 18:56:34 +0300

    Create Principiul cutiei lui Dirichlet.cpp

---
+++ Principiul cutiei lui Dirichlet.cpp
@@ -0,0 +1,28 @@
+// Principiul cutiei lui Dirichlet determina o submultime a unei multime care este divizbila cu numarul de elemente
+
+void dirichlet(int vec[], int n, int& lo, int& hi) {
+    int sum[NMAX] = {0}, rest[NMAX] = {0};
+    for (int i = 1; i <= n; ++ i) {
+        sum[i] = sum[i - 1] + vec[i];
+        if (sum[i] % n == 0) {
+            lo = 1;
+            hi = n;
+            return;
+        }
+        else {
+            if (rest[sum[i] % n]) {
+                lo = rest[sum[i] % n] + 1;
+                hi = i;
+                return;
+            }
+            else 
+                rest[sum[i] % n] = i;
+        }
+    } 
+}
+
+
+/* Observatii: 
+1. Exista intotdeauna solutie.
+2. Solutia este formata din elementele de pe pozitiile lo, lo + 1 ... hi - 1, hi.
+*/
