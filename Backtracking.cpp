a02bb2ca108693f5e3c5a9137e7cc0e4029c4bd9
Daniel Sava <danyvs_99@yahoo.com>
2017-07-18 10:32:47 +0300

    Create Backtracking.cpp

---
+++ Backtracking.cpp
@@ -0,0 +1,11 @@
+/* Pentru ca o problema sa poate fi rezolvata cu ajutorul metodei backtracking, solutia trebuie sa poata fi reprezentata cu ajutorul unui
+vector, fiecare element al vectorului trebuie sa ia valori intr-o multime ordonata si fininta.
+Solutia problemei se contruieste pas cu pas, la fiecare pas verificandu-se daca solutia partiala este valida.
+Metoda backtracking genereaza, de fapt, toate elementele prdosului carteziana unei multimi, renuntand din timp la toate combinatiile care 
+nu pot fi solutii finale.
+*/
+
+// 1. Schema generala
+void bkt(int k) {
+
+}
