18204fcd12a1bbe6427d0eae5908b7914f5a2dd0
Daniel Sava <danyvs_99@yahoo.com>
2017-07-18 10:53:27 +0300

    Update Backtracking.cpp

--- Backtracking.cpp
+++ Backtracking.cpp
@@ -6,6 +6,81 @@ nu pot fi solutii finale.
 */
 
 // 1. Schema generala
-void bkt(int k) {
-
+void bkt(int k, int X[], int a, int b) {
+    for (int i = a; i <= b; ++ i) {
+        X[k] = i;
+        if (valid(k)) {
+            if (finalSolution(k))
+                print(k);
+            else
+                bkt(k + 1, X, a, b);
+        }
+    }
 }
+
+
+// 2. Permutari
+// - numar solutii = n!
+void bkt(int k, int X[], int n, bool seen[]) {
+    for (int i = 1; i <= n; ++ i)
+        if (!seen[i]) {
+            X[k] = i;
+            seen[i] = true;
+            if (k == n) {
+                // prelucrare solutie
+            }
+            else
+                bkt(k + 1, X, n, seen);
+            seen[i] = false;
+        }
+}
+/* Observatii:
+1. Vectorul seen trebuie initializat cu false.
+2. Apelul functiei in main este de forma bkt(1, X, n, seen).
+*/
+
+
+// 3. Aranjamente
+// - numar solutii = n! / (n - m)!
+void bkt(int k, int X[], int n, int m, bool seen[]) {
+    for (int i = 1; i <= n; ++ i)
+        if (!seen[i]) {
+            X[k] = i;
+            seen[i] = true;
+            if (k == m) {
+                // prelucrare solutie
+            }
+            else
+                bkt(k + 1, X, n, m, seen);
+            seen[i] = false;
+        }
+}
+
+/* Observatii:
+1. Vectorul seen trebuie initializat cu false.
+2. Apelul functiei in main este de forma bkt(1, X, n, m, seen).
+3. Ordinea elementelor are importanta.
+*/
+
+
+// 4. Combinari
+// - numar solutii = n! / (m! * (n - m)!)
+void bkt(int k, int X[], int n, int m) {
+    for (int i = X[k - 1] + 1; i <= n - m + k; ++ i) {
+            X[k] = i;
+            if (k == m) {
+                // prelucrare solutie
+            }
+            else
+                bkt(k + 1, X, n, m);
+        }
+}
+/* Observatii: 
+1. Vectorul seen nu mai este necesar.
+2. X[0] trebuie initilizat cu 0 in main.
+3. Apelul functiei in main este de forma bkt(1, n, m).
+4. Ordinea elementelor nu are importanta.
+*/
+
+
+
