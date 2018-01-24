b2f6b4c7aef26eb622ab8014189ba3d2cd94bb94
Daniel Sava <danyvs_99@yahoo.com>
2018-01-24 10:45:26 +0200

    Create 03. Algoritmul lui Roy-Floyd.cpp

---
+++ Teoria Grafurilor/Grafuri ponderate/03. Algoritmul lui Roy-Floyd.cpp
@@ -0,0 +1,9 @@
+  /* Algoritmul lui Roy-floyd determina drumurile de cost minim/ maxim intre oricare 2 noduri ale grafului, transformand matricea costurilor ascoata acestuia. */
+  
+void royFloyd(int A[NMAX][NMAX], int n) {
+    for (int k = 1; k <= n; ++ k)
+        for (int i = 1; i <= n; ++ i)
+            for (int j = 1; j <= n; ++ j)
+                if (A[i][k] && A[k][j] && (A[i][j] > A[i][k] + A[k][j] || !A[i][j]) && i != j)
+                    A[i][j] = A[i][k] + A[k][j];
+}
