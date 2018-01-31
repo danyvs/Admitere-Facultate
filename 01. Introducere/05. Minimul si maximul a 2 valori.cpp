81c459da16df7e1dd9f2b511d150c8edda6a222b
Daniel Sava <danyvs_99@yahoo.com>
2018-01-31 17:34:27 +0200

    Create 05. Minimul si maximul a 2 valori.cpp

---
+++ 01. Introducere/05. Minimul si maximul a 2 valori.cpp
@@ -0,0 +1,10 @@
+// MINIMUL A 2 VALORI
+inline int min(const int& a, const int& b) {
+    return (a < b) ? a : b;
+}
+
+
+// MAXIMUL A 2 VALORI
+inline int max(const int& a, const int& b) {
+    return (a > b) ? a : b;
+}
