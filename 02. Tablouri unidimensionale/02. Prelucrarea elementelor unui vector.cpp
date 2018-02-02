4bba0aaf4d3c010de4cda87d2bdad2c31abf4d75
Daniel Sava <26028086+danyvs@users.noreply.github.com>
2018-02-02 19:24:08 +0200

    Update 02. Prelucrarea elementelor unui vector.cpp

--- 02. Tablouri unidimensionale/02. Prelucrarea elementelor unui vector.cpp
+++ 02. Tablouri unidimensionale/02. Prelucrarea elementelor unui vector.cpp
@@ -1,16 +1,28 @@
-// 1. Prelucrarea tuturor elementelor unui vector
-  for (int i = 1; i <= n; ++ i)
-      // prelucrare a[i]
-      
-// 2.Prelucrarea perechilor de elemente aflat pe pozitii consecutive
-  for (int i = 1; i < n; ++ i)
-      // prelucrare (a[i], a[i + 1])
-      
-// 3. Prelucrarea elementelor egale departate de mijloc
-  for (int i = 1; i <= n / 2; ++ i)
-      // prelucrare (a[i], a[n - i + 1])
-      
+// 1. Prelucrarea fiecarui element
+void prelElementsOfVector(int vec[], int n) {
+    for (int i = 1; i <= n; ++ i) {
+        // prelucrare vec[i]
+    }
+}
+
+// 2. Prelucrarea perechilor de elemente aflate pe pozitii consecutive
+void prelElementsOfVector(int vec[], int n) {
+    for (int i = 1; i < n; ++ i) {
+        // prelucrare (vec[i], vec[i + 1])
+    }
+}
+
+// 3. Prelucrarea perechilor de elemente aflate pe pozitii egal departate de mijloc
+void prelElementsOfVector(int vec[], int n) {
+    for (int i = 1; i <= n / 2; ++ i) {
+        // prelucrare (vec[i], vec[n - i + 1])
+    }
+}
+
 // 4. Prelucrarea tuturor perechilor de elemente
-  for (int i = 1; i < n; ++ i)
-      for (int j = i + 1; j <= n; ++ j)
-          // prelucrare (a[i], a[j])  
+void prelElementsOfVector(int vec[], int n) {
+    for (int i = 1; i < n; ++ i)
+        for (int j = i + 1; j <= n; ++ j) {
+            // prelucrare (vec[i], vec[j])
+        }
+}
