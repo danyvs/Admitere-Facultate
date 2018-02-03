63eec95208b84c7ffd50f3a311569112f34d852a
Daniel Sava <26028086+danyvs@users.noreply.github.com>
2018-02-03 11:52:50 +0200

    Update and rename 03. Cautarea unui numar intr-un vector.cpp to 01. Cautarea unui numar intr-un vector.cpp

---
+++ 02. Tablouri unidimensionale/01. Cautarea unui numar intr-un vector.cpp
@@ -0,0 +1,30 @@
+// 1. Cautare secventiala 
+// - se poate aplica pe orice tip de vector
+// - complexitate: O(n)
+bool naiveSearch(int vec[], int n, int target) {
+    for (int i = 1; i <= n; ++ i)
+        if (vec[i] == target)
+            return true;
+    return false;
+}
+
+// 2. Cautare binara
+// - se poate aplica doar pe un vector sortat
+// - complexitate: O(log2n)
+bool binarySearch(int vec[], int n, int target) {
+    int lo = 1, hi = n;
+    while (lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (vec[mid] == target)
+            return true;
+        else {
+            if (vec[mid] > target)
+                hi = mid - 1;
+            else 
+                lo = mid + 1;
+        }
+    }
+    return false;
+}
+
+// Vectorii sunt indexati de la 1
