3d24d38a08c915ccff1c2b3bdea66bd6c0f32646
Daniel Sava <26028086+danyvs@users.noreply.github.com>
2018-02-03 11:36:15 +0200

    Update and rename 11. Test Numar Prim.cpp to 05. Test Numar Prim.cpp

---
+++ 01. Introducere/05. Test Numar Prim.cpp
@@ -0,0 +1,15 @@
+bool prime(int x) {
+    if (x < 2)
+        return false;
+    if (x == 2)
+        return true;
+    if (x % 2 == 0)
+        return false;
+    for (int i = 3; i * i <= x; i += 2)
+        if (x % i == 0)
+            return false;
+    return true;
+}
+
+/* Daca este necesara verificarea proprietatii de numar prim pentru un numar mare de numere, este recomandat sa se foloseasca Ciurul lui
+Eratostene */
