aab6385c243458c391146deda93ef3d364b3b6fd
Daniel Sava <26028086+danyvs@users.noreply.github.com>
2018-02-03 11:34:12 +0200

    Create 04. Prelucrarea divizorilor unui numar.cpp

---
+++ 01. Introducere/04. Prelucrarea divizorilor unui numar.cpp
@@ -0,0 +1,15 @@
+// complexitate: O(n)
+  for (int i = 1; i <= n; ++ i)
+      if (n % i == 0)
+          // prelucrare i
+          
+          
+// complexitate: O(sqrt{n})
+  int i = 1;
+  for (i = 1; i * i < n; ++ i)
+      if (n % i == 0)
+          // prelucrare i si n / i
+  if (i * i == n) {
+      // n este patrat perfect
+      // prelucrare i
+  }
