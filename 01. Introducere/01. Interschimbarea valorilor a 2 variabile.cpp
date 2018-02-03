a51f07b03b818f9679f12a0aa40301555e3c17bd
Daniel Sava <26028086+danyvs@users.noreply.github.com>
2018-02-03 11:22:44 +0200

    Update 01. Interschimbarea valorilor a 2 variabile.cpp

--- 01. Introducere/01. Interschimbarea valorilor a 2 variabile.cpp
+++ 01. Introducere/01. Interschimbarea valorilor a 2 variabile.cpp
@@ -3,7 +3,3 @@ void swap(int& a, int& b) {
     a = b;
     b = temp;
 }
-
-/* Functia swap poate interschimba valorile si a altor tipuri de date. Singura conditie este ca atribuirea pentru acel tip sa se poate
-face cu =. De exemplu, toate tipurile numerice se pot interschimba dupa modelul de mai sus. Insa, sirurile de caractere nu se poate 
-interschimba, deoarece copierea se face cu ajutorul functiilor specifice. */
