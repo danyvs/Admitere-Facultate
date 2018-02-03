766b85abc0edec68119e534a8f99639a95726d84
Daniel Sava <26028086+danyvs@users.noreply.github.com>
2018-02-03 11:40:28 +0200

    Update and rename 01. Introducere/03. Cel mai mare divizor comun.cpp to 01. Algoritmi elementari/02. Divizibilitate/03. Cel mai mare divizor comun.cpp

--- 01. Algoritmi elementari/02. Divizibilitate/03. Cel mai mare divizor comun.cpp
+++ 01. Algoritmi elementari/02. Divizibilitate/03. Cel mai mare divizor comun.cpp
@@ -28,8 +28,6 @@ int gcd(int a, int b) {
     return (!b) ? a : gcd(b, a % b);
 }
 
-/* Observatii
-1. Cel mai mic multiplu comun a 2 numere se calculeaza dupa urmatoarea formula: cmmmc(a, b) = (a * b) / cmmdc(a, b).
-2. Pentru a verifica daca 2 numere sunt prime intre ele, se calculeaza valoarea cmmdc-ului: daca aceasta este 1, atunci numerele sunt
-prime intre ele; daca valoarea este diferita de 1, atunci numerele NU sunt prime intre ele.
-*/
+// 1. Cel mai mic multiplu comun a 2 numere se calculeaza dupa urmatoarea formula: cmmmc(a, b) = (a * b) / cmmdc(a, b).
+/* 2. Pentru a verifica daca 2 numere sunt prime intre ele, se calculeaza valoarea cmmdc-ului: daca aceasta este 1, atunci numerele sunt
+prime intre ele; daca valoarea este diferita de 1, atunci numerele NU sunt prime intre ele. */
