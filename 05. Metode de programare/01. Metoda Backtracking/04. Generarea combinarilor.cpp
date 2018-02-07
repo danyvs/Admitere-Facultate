f9ee13e3add82db54ae722d46405720c63d4fb4d
Daniel Sava <26028086+danyvs@users.noreply.github.com>
2018-02-07 11:11:00 +0200

    Update 04. Generarea combinarilor.cpp

--- 05. Metode de programare/01. Metoda Backtracking/04. Generarea combinarilor.cpp
+++ 05. Metode de programare/01. Metoda Backtracking/04. Generarea combinarilor.cpp
@@ -10,13 +10,12 @@ void bkt(int k, int X[], int n, int m) {
 }
 
 
-/* 1. Numar solutii: Cnm = n! / (m! * (n - m)!)
-      Pentru n = 4 si m = 3 => combinarile, IN ORDINE LEXICOGRAFICA, sunt : 1 2 3
-                                                                            1 2 4
-                                                                            1 3 4
-                                                                            2 3 4
+/* 
+1. Numar solutii: Cnm = n! / (m! * (n - m)!)
+   Pentru n = 4 si m = 3 => combinarile, IN ORDINE LEXICOGRAFICA, sunt : 1 2 3
+                                                                         1 2 4
+                                                                         1 3 4
+                                                                         2 3 4
 2. Ordinea elementelor NU are importanta.
 3. X[0] trebuie initilizat cu 0 in main.
 */
-
-
