5b3397464969621dd05c13348cdc1819807f9000
Daniel Sava <26028086+danyvs@users.noreply.github.com>
2018-02-07 11:15:38 +0200

    Update 07. Generarea partitiilor unui numar.cpp

--- 05. Metode de programare/01. Metoda Backtracking/07. Generarea partitiilor unui numar.cpp
+++ 05. Metode de programare/01. Metoda Backtracking/07. Generarea partitiilor unui numar.cpp
@@ -17,7 +17,12 @@ void bkt(int k, int n, int X[], int sum) {
 
 
 /* 
-1. X[0] trebuie initializat cu 1.
-2. Variabila sum trebuie initializata in main cu 0.
-3. Variabila sum memoreaza suma numerelor pana la pasul curent.
+1. Pentru n = 4, partiriile lui n, IN ORDINE LEXICOGRAFICA, sunt: 1 + 1 + 1 + 1
+                                                                  1 + 1 + 2
+                                                                  1 + 3
+                                                                  2 + 2
+                                                                  4
+2. X[0] trebuie initializat cu 1.
+3. Variabila sum trebuie initializata in main cu 0.
+4. Variabila sum memoreaza suma numerelor pana la pasul curent.
 */
